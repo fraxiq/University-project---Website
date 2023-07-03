4dfd9d0f17b1354ac887b0ad1919c13ea02fdc96
fraxiq <alexns007@abv.bg>
2023-07-03 04:20:38 +0300

    Upload

---
+++ kursov proekt - Website/kursov proekt/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("kursov_proekt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("kursov_proekt")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("64bb2349-dcc4-48c5-949c-c3ea4bef9353")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ kursov proekt - Website/kursov proekt/script.js
@@ -0,0 +1,5 @@
+﻿gsap.from('.picture1', { duration: 3, x: '-100%', ease: 'bounce' })
+gsap.from('.aboutpic1', { duration: 3, x: '-100%', ease: 'bounce' })
+gsap.from('.picture2', { duration: 4, x: '100%', ease: 'elastic' })
+gsap.from('.text', { duration: 3, y: '100%', ease: 'power3' })
+gsap.from('.item', { duration: 2, y: '50%', ease: 'power2' })
